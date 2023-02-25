dc601b7b9346583fedad596680df45e895cbd495
Atilla Sertkaya <a.sertkaya@celonis.de>
2023-02-25 18:08:28 +0100

    publish

--- index.js
+++ index.js
@@ -1,16 +1,71 @@
 const glob = require('glob')
 const fs = require('fs')
+const shell = require('shelljs');
+
+const template = `
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Component Report</title>
+    <style>
+        html,body {
+            font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+            color: #0a1f44;
+        }
+        .container {
+            width: 1200px;
+            margin: 0 auto;
+        }
+        table {
+            width: 100%;
+        }
+        table,
+        td {
+            border-top: 1px solid #ccc;
+            border-bottom: 1px solid #ccc;
+            border-collapse: collapse;
+            background: #fafafd;
+        }
+
+        td {
+            padding: 10px 20px;
+        }
+
+        td a {
+          white-space: no-wrap;
+        }
+        
+    </style>
+</head>
+
+<body>
+    <div class="container">
+    __PLACEHOLDER__
+    </div>
+</body>
+
+</html>
+`
 
 const getGithubLink = (file, line)=> {
   return `https://github.com/celonis/ems-frontend/blob/main/${file}#L${line}`
 }
 
-const component = process.argv[2];
-const report = [];
+export function search(args) {
+  const component = args[2];
+  
+  if(!component) {
+    throw new Error("Please provide a component name");
+  }
 
-function search() {
+
+  const report = [];
+
   const g = glob('{,!(node_modules)/**/}*.html', (err, files) => {
-    console.log("glob")
     if (err) {
       console.log('Error', err)
     } else {
@@ -31,31 +86,55 @@ function search() {
       })
     }
   })
+
+  function groupByFile() {
+    const groupedReport = {};
+
+    report.forEach((item)=> {
+      const { file } = item;
+      if(groupedReport[file]) {
+        groupedReport[file].push(item);
+      } else {
+        groupedReport[file] = [item];
+      }
+    });
+
+    return groupedReport;
+  }
   
   g.addListener("end", ()=> {
-    const template = fs.readFileSync('template.html').toString();
-  
+    const groupedReport = groupByFile(report);
     const html = [];
   
     html.push(`<h1>${component}</h1>`);
+
+    html.push(`<p><strong>${component}</strong> component is used in ${report.length} places.</p>`)
   
     html.push("<div class='table-container'>");
     html.push("<table>");
   
-    report.forEach(({file, line, url})=> {
-      html.push("<tr>");
+    Object.keys(groupedReport).forEach((file)=> {
+      const report = groupedReport[file];
+
+      html.push(`<tr><td colspan='2'><strong>${file}</strong></td></tr>`);
+      
+      report.forEach(({file, line, url})=> {
+        html.push("<tr>");
   
-      html.push(`<td>${file}</td>`);
-      html.push(`<td><a href="${url}" target="_blank">See File</a></td>`);
-  
-      html.push("</tr>");
+        html.push(`<td>Line ${line}</td>`);
+        html.push(`<td align="end"><a href="${url}" target="_blank">See File</a></td>`);
+    
+        html.push("</tr>");
+      })
     });
   
     html.push("</table>");
     html.push("</div>");
   
     fs.writeFileSync('report.html', template.replace("__PLACEHOLDER__", html.join("")));
+
+    shell.echo('The report is generated...');
+    shell.exec('open ./report.html');
   })
 }
 
-module.exports = search;
